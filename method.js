2721ac15f16859d8ab2ff2c1f061a1959a9eba8b
joaoneves13 <13joaoneves@gmail.com>
2019-11-08 14:35:07 +0100

    Functions in Object

---
+++ method.js
@@ -0,0 +1,14 @@
+const chef = {
+    name: 'Rein',
+    chefName: 'Jeremiah Bocuse',
+    age: 30,
+    cook: function() {
+        console.log('I am cooking')
+    },
+    speak: function() {
+        console.log("Hello, how are you?");
+    }
+  }
+
+  chef.speak()
+  
