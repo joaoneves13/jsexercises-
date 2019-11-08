413e9895404f7d0c484d427406a56a43dc51dc2e
joaoneves13 <13joaoneves@gmail.com>
2019-11-08 13:41:40 +0100

    Reuseable functions

---
+++ reusablefunc.js
@@ -0,0 +1,14 @@
+//function learnTopic(){
+  //  console.log("We are learning functions");
+    
+//}
+//learnTopic()
+
+function learnTopic(topic){
+    console.log("We are learning" + " " + topic);
+    
+}
+learnTopic("functions")
+learnTopic("scope")
+learnTopic("loops")
+learnTopic("conditionals")
